df8a5285defd4e04f93d1a811f026f4027380cdf
zekroTJA <zekroTJA@users.noreply.github.com>
2025-11-21 18:49:50 +0000

    deploy: 5aa4c68194bb84333e480b7b22bebd613d538a00

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1e520e38.js',
+                path_to_root + 'searchindex-7680b46c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
